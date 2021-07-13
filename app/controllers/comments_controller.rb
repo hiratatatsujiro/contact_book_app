319bcbeba30973e620d42c30512ef390f16a3c82
hiratatatsujiro <i_am_3312in_meizen@yahoo.co.jp>
2021-07-14 05:11:39 +0900

    コメント非同期通信機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
 
   def create
+<<<<<<< Updated upstream
     @diary = Diary.find(params[:diary_id])
     @comment = Comment.new(comment_params)
     if @comment.valid? 
@@ -10,11 +11,19 @@ class CommentsController < ApplicationController
       @diary = Diary.find(params[:diary_id])
       @comment = Comment.new(comment_params)
       render "diaries/show"
+=======
+    @diary = Diary.find(params[:diary_id]) #①
+    @comment = @diary.comments.build(comment_params) #②
+    @comment.user_id = current_user.id #③
+    if @comment.save
+      render :index #④
+>>>>>>> Stashed changes
     end
   end
 
+
   private
-  def comment_params
-    params.require(:comment).permit(:comment).merge(user_id: current_user.id, diary_id: params[:diary_id])
-  end
+    def comment_params
+      params.require(:comment).permit(:comment, :diary_id, :user_id)
+    end
 end
