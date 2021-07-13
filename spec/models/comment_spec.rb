5d4edb67c5b279155a5a1684868d32792a8f48f7
hiratatatsujiro <i_am_3312in_meizen@yahoo.co.jp>
2021-07-14 08:32:19 +0900

    コメント単体テスト

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  context '内容に問題がない場合' do
+    it '全ての情報があれば登録できる' do
+      expect(@comment).to be_valid
+    end
+  end
+
+  context '内容に問題がある場合' do
+    it 'commentが空だと登録できない' do
+      @comment.comment = ""
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include("Comment can't be blank")  
+    end
+
+    it 'userが紐付いていないと登録できない' do
+      @comment.user = nil
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include("User must exist")  
+    end
+
+    it 'diaryが紐付いていないと登録できない' do
+      @comment.diary = nil
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include("Diary must exist")  
+    end
+    
+  end
 end
