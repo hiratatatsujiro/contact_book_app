5095970ded9e3618ed19544cb30f148c499fd1be
hiratatatsujiro <i_am_3312in_meizen@yahoo.co.jp>
2021-07-14 11:05:51 +0900

    チャットルーム単体テスト

--- spec/models/chat_message_spec.rb
+++ spec/models/chat_message_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe ChatMessage, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "create" do
+    before do
+      @chat_message = FactoryBot.build(:chat_message)
+    end
+    it 'contentとimageが存在していれば保存できること' do
+      expect(@chat_message).to be_valid
+    end
+
+    it 'contentが空でも保存できること' do
+      @chat_message.content = ""
+      expect(@chat_message).to be_valid
+    end
+
+    it 'imageが空でも保存できること' do
+      @chat_message.image = nil
+      expect(@chat_message).to be_valid
+    end
+
+    it 'contentとimageが空では保存できないこと' do
+      @chat_message.content = ""
+      @chat_message.image = nil
+      @chat_message.valid?
+      expect(@chat_message.errors.full_messages).to include("Content can't be blank")
+    end
+
+    it 'roomが紐付いていないと保存できないこと' do
+      @chat_message.room = nil
+      @chat_message.valid?
+      expect(@chat_message.errors.full_messages).to include("Room must exist")
+    end
+
+    it 'userが紐付いていないと保存できないこと' do
+      @chat_message.user = nil
+      @chat_message.valid?
+      expect(@chat_message.errors.full_messages).to include("User must exist")
+    end
+
+  end
 end
