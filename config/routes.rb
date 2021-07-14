abd451e2cac2f250b549765f87f99aa9a134a9d8
hiratatatsujiro <i_am_3312in_meizen@yahoo.co.jp>
2021-07-14 09:43:09 +0900

    ビューの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'chat_messages/index'
   devise_for :users
   root to: "home#top"
   resources :diaries do
     resources :comments, only: :create
   end
   resources :users, only: [:show, :edit, :update]
+  resources :rooms, only: [:index, :new, :create, :destroy] do
+    resources :chat_messages, only: [:index, :create]
+  end
 end
