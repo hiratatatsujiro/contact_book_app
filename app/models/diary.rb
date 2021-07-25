168b800b9cf5e734869d4b900b7401312b446fd3
hiratatatsujiro <i_am_3312in_meizen@yahoo.co.jp>
2021-07-25 18:17:51 +0900

    モデル調整

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -2,6 +2,4 @@ class Diary < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_many :comments, dependent: :destroy
-  has_many :diary_tag_relations, dependent: :destroy
-  has_many :tags, through: :diary_tag_relations
 end
