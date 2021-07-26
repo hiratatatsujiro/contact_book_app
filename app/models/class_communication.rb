e0a98029c6fdafc856a13600f305d53b1340023e
hiratatatsujiro <i_am_3312in_meizen@yahoo.co.jp>
2021-07-26 12:55:25 +0900

    Update class_communication.rb

--- app/models/class_communication.rb
+++ app/models/class_communication.rb
@@ -1,11 +1,5 @@
 class ClassCommunication < ApplicationRecord
   belongs_to :user
   has_many_attached :images
-<<<<<<< HEAD
   has_many :likes, dependent: :destroy
-=======
-  has_many :likes
-  has_many :tag_class_communications, dependent: :destroy
-  has_many :tags, through: :tag_class_communications
->>>>>>> master
 end
