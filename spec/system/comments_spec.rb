ab34011e33deaa93a80bf7e8f83afa0c9a28bfc0
hiratatatsujiro <i_am_3312in_meizen@yahoo.co.jp>
2021-08-21 22:28:25 +0900

    timetable結合テスト

---
+++ spec/system/comments_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'コメント投稿', type: :system do
+  before do
+    @diary = FactoryBot.create(:diary)
+    @comment = Faker::Lorem.sentence
+  end
+
+  it 'ログインしたユーザーは日記詳細ページでコメント投稿できる' do
+    # ログインする
+    visit new_user_session_path
+    fill_in 'user[email]', with: @diary.user.email
+    fill_in 'user[password]', with: @diary.user.password
+    find('input[name="commit"]').click
+    expect(current_path).to eq(root_path)
+    # 日記一覧ページに遷移する
+    visit diaries_path
+    # 日記詳細ボタンが存在する
+    expect(page).to have_link @diary.diary_day, href: diary_path(@diary)
+    # ツイート詳細ページに遷移する
+    visit diary_path(@diary)
+    # フォームに情報を入力する
+    fill_in "comment[comment]", with: @comment
+    # コメントを送信すると、Commentモデルのカウントが1上がることを確認する
+    expect{
+      find('input[name="commit"]').click
+    }.to change { Comment.count }.by(1)
+    # 詳細ページにリダイレクトされることを確認する
+    expect(current_path).to eq diary_path(@diary)
+    # 詳細ページ上に先ほどのコメント内容が含まれていることを確認する
+    expect(page).to have_content(@comment)
+  end
+end
+
--- spec/system/timetables_spec.rb
+++ spec/system/timetables_spec.rb
@@ -2,137 +2,162 @@ require 'rails_helper'
 
 RSpec.describe 'Timetables', type: :system do
   before do
-    @Timetable = FactoryBot.create(:timetable)
+    @timetable = FactoryBot.create(:timetable)
   end
 
   context '連絡投稿ができるとき'do
     it 'ログインしたユーザーは新規投稿できる' do
       # ログインする
       visit new_user_session_path
-      fill_in 'user[email]', with: @user.email
-      fill_in 'user[password]', with: @user.password
+      fill_in 'user[email]', with: @timetable.user.email
+      fill_in 'user[password]', with: @timetable.user.password
       find('input[name="commit"]').click
       expect(current_path).to eq(root_path)
       # 連絡一覧ページへのボタンがあることを確認する
-      expect(page).to have_content('連絡する')
+      expect(page).to have_content('時間わり')
       # 連絡一覧ページに移動する
-      visit contacts_path
+      visit timetables_path
       # 新規投稿ボタンがあることを確認する
-      expect(page).to have_content('連絡する')
+      expect(page).to have_content('時間わり作成')
       # 新規投稿ページに移動する
-      visit new_contact_path
+      visit new_timetable_path
       # フォームに情報を入力する
-      select @contact.condition.name, from: 'contact[condition_id]'
-      fill_in 'contact[reason]', with: @contact.reason
-      select @contact.pool_marathon.name, from: 'contact[pool_marathon_id]'
-      fill_in 'contact[contact]', with: @contact.contact
+      select 2021 ,from: 'timetable[next_day(1i)]'
+      select 4 ,from: 'timetable[next_day(2i)]'
+      select 4 ,from: 'timetable[next_day(3i)]'
+      select @timetable.first_class.name, from: 'timetable[first_class_id]'
+      select @timetable.second_class.name, from: 'timetable[second_class_id]'
+      select @timetable.third_class.name, from: 'timetable[third_class_id]'
+      select @timetable.fourth_class.name, from: 'timetable[fourth_class_id]'
+      select @timetable.fifth_class.name, from: 'timetable[fifth_class_id]'
+      select @timetable.sixth_class.name, from: 'timetable[sixth_class_id]'
+      fill_in 'timetable[leave_time]', with: @timetable.leave_time
+      fill_in 'timetable[homework]', with: @timetable.homework
+      fill_in 'timetable[preparation]', with: @timetable.preparation
+      fill_in 'timetable[notice]', with: @timetable.notice
       # 送信するとcontactモデルのカウントが1上がることを確認する
       expect{
         find('input[name="commit"]').click
-      }.to change { Contact.count }.by(1)
+      }.to change { Timetable.count }.by(1)
       # 連絡一覧ページに遷移することを確認する
-      expect(current_path).to eq(contacts_path)
+      expect(current_path).to eq(timetables_path)
       # トップページには先ほど投稿した内容の連絡が存在することを確認する（日付）
-      expect(page).to have_content(@contact.created_at.strftime('%Y/%m/%d %H:%M'))
+      expect(page).to have_content(@timetable.next_day)
     end
   end
 
-  context '連絡投稿ができないとき'do
-    it 'ログインしていないと新規投稿ページに遷移できない' do
+  context '時間わり投稿ができないとき'do
+    it 'ログインしていないと時間わり投稿ページに遷移できない' do
      # トップページにいる
      visit root_path
      # 連絡一覧ページへのボタンがあることを確認する
-     expect(page).to have_content('連絡する')
+     expect(page).to have_content('時間わり')
      # 連絡一覧ページに移動する
-     visit contacts_path
+     visit timetables_path
      # ログインページへ戻されることを確認する
      expect(current_path).to eq(new_user_session_path)
     end
   end
 end
 
-RSpec.describe '連絡編集', type: :system do
+RSpec.describe '時間わり編集', type: :system do
   before do
-    @contact = FactoryBot.create(:contact)
-    author = @contact.user
-    @user2 = FactoryBot.create(:user, grade_id: author.grade_id, classroom_id: author.classroom_id, number_id: 52)
+    @timetable = FactoryBot.create(:timetable, next_day: "2021-05-04")
   end
 
   context '連絡編集ができるとき' do
     it 'ログインしたユーザーは自分が投稿した連絡の編集ができる' do
-      # 連絡を投稿したユーザーでログインする
+      # ログインする
       visit new_user_session_path
-      fill_in 'user[email]', with: @contact.user.email
-      fill_in 'user[password]', with: @contact.user.password
+      fill_in 'user[email]', with: @timetable.user.email
+      fill_in 'user[password]', with: @timetable.user.password
       find('input[name="commit"]').click
       expect(current_path).to eq(root_path)
       # 連絡一覧ページへのボタンがあることを確認する
-      expect(page).to have_content('連絡する')
+      expect(page).to have_content('時間わり')
       # 連絡一覧ページに移動する
-      visit contacts_path
+      visit timetables_path
       # 連絡詳細ボタンがあることを確認する
-      expect(page).to have_link @contact.created_at.strftime('%Y/%m/%d %H:%M'), href: contact_path(@contact)
+      expect(page).to have_link @timetable.next_day, href: timetable_path(@timetable)
       # 連絡詳細ページに移動する
-      visit contact_path(@contact)
+      visit timetable_path(@timetable)
       # 連絡に「編集」へのリンクがあることを確認する
-      expect(page).to have_link '編集', href: edit_contact_path(@contact)
+      expect(page).to have_link '編集', href: edit_timetable_path(@timetable)
       # 編集ページへ遷移する
-      visit edit_contact_path(@contact)
+      visit edit_timetable_path(@timetable)
       # すでに投稿済みの内容がフォームに入っていることを確認する
       expect(
-        find('#condition').value
-      ).to eq(@contact.condition.id.to_s)
+        find('#timetable_next_day_1i').value
+      ).to eq("2021")
       expect(
-        find('#reason').value
-      ).to eq(@contact.reason)
+        find('#timetable_next_day_2i').value
+      ).to eq("5")
       expect(
-        find('#pool_marathon').value
-      ).to eq(@contact.pool_marathon.id.to_s)
+        find('#timetable_next_day_3i').value
+      ).to eq("4")
       expect(
-        find('#contact').value
-      ).to eq(@contact.contact)
+        find('#timetable-first-class').value
+      ).to eq(@timetable.first_class.id.to_s)
+      expect(
+        find('#timetable-second-class').value
+      ).to eq(@timetable.second_class.id.to_s)
+      expect(
+        find('#timetable-third-class').value
+      ).to eq(@timetable.third_class.id.to_s)
+      expect(
+        find('#timetable-fourth-class').value
+      ).to eq(@timetable.fourth_class.id.to_s)
+      expect(
+        find('#timetable-fifth-class').value
+      ).to eq(@timetable.fifth_class.id.to_s)
+      expect(
+        find('#timetable-sixth-class').value
+      ).to eq(@timetable.sixth_class.id.to_s)
+      expect(
+        find('#leave_time').value
+      ).to eq(@timetable.leave_time)
+      expect(
+        find('#homework').value
+      ).to eq(@timetable.homework)
+      expect(
+        find('#preparation').value
+      ).to eq(@timetable.preparation)
+      expect(
+        find('#notice').value
+      ).to eq(@timetable.notice)
       # 連絡内容を編集する
-      select "お休みします", from: 'contact[condition_id]'
-      fill_in 'contact[reason]', with: @contact.reason + "編集したテキスト"
-      select "できます", from: 'contact[pool_marathon_id]'
-      fill_in 'contact[contact]', with: @contact.contact + "編集したテキスト"
+      select 2022 ,from: 'timetable[next_day(1i)]'
+      select 5 ,from: 'timetable[next_day(2i)]'
+      select 15 ,from: 'timetable[next_day(3i)]'
+      select "国語", from: 'timetable[first_class_id]'
+      select "国語", from: 'timetable[second_class_id]'
+      select "国語", from: 'timetable[third_class_id]'
+      select "国語", from: 'timetable[fourth_class_id]'
+      select "国語", from: 'timetable[fifth_class_id]'
+      select "国語", from: 'timetable[sixth_class_id]'
+      fill_in 'timetable[leave_time]', with: @timetable.leave_time + "編集したテキスト"
+      fill_in 'timetable[homework]', with: @timetable.homework + "編集したテキスト"
+      fill_in 'timetable[preparation]', with: @timetable.preparation + "編集したテキスト"
+      fill_in 'timetable[notice]', with: @timetable.notice + "編集したテキスト"
       # 編集してもTweetモデルのカウントは変わらないことを確認する
       expect{
         find('input[name="commit"]').click
-      }.to change { Contact.count }.by(0)
+      }.to change { Timetable.count }.by(0)
       # 連絡一覧ページに遷移することを確認する
-      expect(current_path).to eq(contacts_path)
+      expect(current_path).to eq(timetable_path(@timetable))
       # トップページには先ほど編集した内容の連絡が存在することを確認する（日付）
-      expect(page).to have_content(@contact.created_at.strftime('%Y/%m/%d %H:%M'))
+      expect(page).to have_content(@timetable.homework)
     end
   end
 
-  context '連絡編集ができないとき' do
-    it 'ログインしたユーザーは自分以外が投稿したツイートの編集画面には遷移できない' do
-      # 別ユーザーでログインする
-      visit new_user_session_path
-      fill_in 'user[email]', with: @user2.email
-      fill_in 'user[password]', with: @user2.password
-      find('input[name="commit"]').click
-      expect(current_path).to eq(root_path)
-      # 連絡一覧ページへのボタンがあることを確認する
-      expect(page).to have_content('連絡する')
-      # 連絡一覧ページに移動する
-      visit contacts_path
-      # 新規投稿ボタンがあることを確認する
-      expect(page).to have_link @contact.created_at.strftime('%Y/%m/%d %H:%M'), href: contact_path(@contact)
-      # 連絡編集ページに移動する
-      visit contact_path(@contact)
-      # 連絡に「編集」へのリンクがないことを確認する
-      expect(page).to have_no_link '編集', href: edit_contact_path(@contact)
-    end
+  context '時間わり編集ができないとき' do
     it 'ログインしていないとツイートの編集画面には遷移できない' do
       # トップページにいる
       visit root_path
       # 連絡一覧ページへのボタンがあることを確認する
-      expect(page).to have_content('連絡する')
+      expect(page).to have_content('時間わり')
       # 連絡一覧ページに移動する
-      visit contacts_path
+      visit timetables_path
       # ログインページへ戻されることを確認する
       expect(current_path).to eq(new_user_session_path)
     end
@@ -141,98 +166,79 @@ end
 
 RSpec.describe '削除', type: :system do
   before do
-    @contact1 = FactoryBot.create(:contact)
-    @contact2 = FactoryBot.create(:contact)
+    @timetable = FactoryBot.create(:timetable)
   end
-  context '連絡削除ができるとき' do
-    it 'ログインしたユーザーは自らが投稿した連絡の削除ができる' do
-      #contact1を投稿したユーザーでログインする
+  context '時間わり削除ができるとき' do
+    it 'ログインしたユーザーは投稿した連絡の削除ができる' do
+      #ログインする
       visit new_user_session_path
-      fill_in 'user[email]', with: @contact1.user.email
-      fill_in 'user[password]', with: @contact1.user.password
+      fill_in 'user[email]', with: @timetable.user.email
+      fill_in 'user[password]', with: @timetable.user.password
       find('input[name="commit"]').click
       expect(current_path).to eq(root_path)
        # 連絡一覧ページへのボタンがあることを確認する
-       expect(page).to have_content('連絡する')
+       expect(page).to have_content('時間わり')
        # 連絡一覧ページに移動する
-       visit contacts_path
+       visit timetables_path
        # 新規投稿ボタンがあることを確認する
-       expect(page).to have_link @contact1.created_at.strftime('%Y/%m/%d %H:%M')
+       expect(page).to have_link @timetable.next_day
        # 新規投稿ページに移動する
-       visit contact_path(@contact1)
+       visit timetable_path(@timetable)
        # 連絡1に「削除」のリンクがあることを確認する
-       expect(page).to have_link '削除', href: contact_path(@contact1)
+       expect(page).to have_link '削除', href: timetable_path(@timetable)
        # 投稿を削除するとレコードの数が1減ることを確認する
        expect{
-       (page).find_link('削除', href: contact_path(@contact1)).click
-       }.to change { Contact.count }.by(-1)
+       (page).find_link('削除', href: timetable_path(@timetable)).click
+       }.to change { Timetable.count }.by(-1)
        # 連絡一覧画面へ遷移する
-       visit contacts_path
+       visit timetables_path
        # トップページには連絡1の内容が存在しないことを確認する
-       expect(page).to have_no_link @contact1.created_at.strftime('%Y/%m/%d %H:%M'), href: contact_path(@contact1)
+       expect(page).to have_no_link @timetable.next_day
     end
   end
-  context '連絡削除ができないとき' do
-    it 'ログインしたユーザーは自分以外が投稿した連絡の削除ができない' do
-      # 連絡1を投稿したユーザーでログインする
-      visit new_user_session_path
-      fill_in 'user[email]', with: @contact1.user.email
-      fill_in 'user[password]', with: @contact1.user.password
-      find('input[name="commit"]').click
-      expect(current_path).to eq(root_path)
-      # 連絡一覧ページへのボタンがあることを確認する
-      expect(page).to have_content('連絡する')
-      # 連絡一覧ページに移動する
-      visit contacts_path
-      # 連絡詳細ボタンがないことを確認する
-      expect(page).to have_no_link @contact2.created_at.strftime('%Y/%m/%d %H:%M'), href: contact_path(@contact2)
-    end
-
-    it 'ログインしていないと連絡削除ボタンがない' do
+  context '時間わり削除ができないとき' do
+    it 'ログインしていないと削除ボタンがない' do
       # トップページにいる
       visit root_path
       # 連絡一覧ページへのボタンがあることを確認する
-      expect(page).to have_content('連絡する')
+      expect(page).to have_content('時間わり')
       # 連絡一覧ページに移動する
-      visit contacts_path
+      visit timetables_path
       # ログインページへ戻されることを確認する
       expect(current_path).to eq(new_user_session_path)
     end
   end
 end
 
-RSpec.describe '連絡詳細', type: :system do
+RSpec.describe '時間わり詳細', type: :system do
   before do
-    @contact = FactoryBot.create(:contact)
+    @timetable = FactoryBot.create(:timetable)
   end
-  it 'ログインしたユーザーはツイート詳細ページに遷移してコメント投稿欄が表示される' do
+  it 'ログインしたユーザーは詳細ページに遷移して編集ボタンが表示される' do
     # ログインする
     visit new_user_session_path
-      fill_in 'user[email]', with: @contact.user.email
-      fill_in 'user[password]', with: @contact.user.password
-      find('input[name="commit"]').click
-      expect(current_path).to eq(root_path)
+    fill_in 'user[email]', with: @timetable.user.email
+    fill_in 'user[password]', with: @timetable.user.password
+    find('input[name="commit"]').click
+    expect(current_path).to eq(root_path)
     # 連絡一覧ページへのボタンがあることを確認する
-    expect(page).to have_content('連絡する')
+    expect(page).to have_content('時間わり')
     # 連絡一覧ページに移動する
-    visit contacts_path
+    visit timetables_path
     # 連絡詳細ボタンがあることを確認する
-    expect(page).to have_link @contact.created_at.strftime('%Y/%m/%d %H:%M')
+    expect(page).to have_link @timetable.next_day
     # 詳細ページに遷移する
-    visit contact_path(@contact)
+    visit timetable_path(@timetable)
     # ページに情報が存在する
-    expect(page).to have_content("#{@contact.condition.name}")
-    expect(page).to have_content("#{@contact.reason}")
-    expect(page).to have_content("#{@contact.pool_marathon.name}")
-    expect(page).to have_content("#{@contact.contact}")
+    expect(page).to have_content("編集する")
   end
   it 'ログインしていない状態で連絡詳細ページに遷移できない' do
     # トップページに移動する
     visit root_path
     # 連絡一覧ページへのボタンがあることを確認する
-    expect(page).to have_content('連絡する')
+    expect(page).to have_content('時間わり')
     # 連絡一覧ページに移動する
-    visit contacts_path
+    visit timetables_path
     # ログインページへ戻されることを確認する
     expect(current_path).to eq(new_user_session_path)
     end
