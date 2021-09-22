bf7825768f41687d432bd316fef343b691f99203
hiratatatsujiro <i_am_3312in_meizen@yahoo.co.jp>
2021-09-22 14:31:07 +0900

    Update comments_spec.rb

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe 'コメント投稿', type: :system do
   before do
     @diary = FactoryBot.create(:diary)
-    @comment = Faker::Lorem.sentence
   end
 
   it 'ログインしたユーザーは日記詳細ページでコメント投稿できる' do
@@ -20,7 +19,7 @@ RSpec.describe 'コメント投稿', type: :system do
     # 日記詳細ページに遷移する
     visit diary_path(@diary)
     # フォームに情報を入力する
-    fill_in "comment[comment]", with: @comment
+    fill_in "comment_comment", with: @comment
     # コメントを送信すると、Commentモデルのカウントが1上がることを確認する
     expect{
       find('input[name="commit"]').click
