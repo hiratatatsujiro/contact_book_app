161d1923302be2bf0a5605455d1bbb0bff103482
hiratatatsujiro <i_am_3312in_meizen@yahoo.co.jp>
2021-09-24 19:54:33 +0900

    Update item_comments_spec.rb

--- spec/system/item_comments_spec.rb
+++ spec/system/item_comments_spec.rb
@@ -1,33 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe '商品コメント', type: :system do
-  before do
-    @item = FactoryBot.create(:item)
-    @item_comment = FactoryBot.create(:item_comment)
-  end
+  let!(:item_comment){FactoryBot.create(:item_comment)}
+  let!(:item){FactoryBot.create(:item)}
 
   it 'ログインしたユーザーは商品詳細ページでコメント投稿できる' do
     # ログインする
     visit new_user_session_path
-    fill_in 'user[email]', with: @item.user.email
-    fill_in 'user[password]', with: @item.user.password
+    fill_in 'user[email]', with: item.user.email
+    fill_in 'user[password]', with: item.user.password
     find('input[name="commit"]').click
     expect(current_path).to eq(root_path)
     # 商品一覧ページに遷移する
     visit items_path
     # 商品詳細ボタンが存在する
-    expect(page).to have_link @item.name, href: item_path(@item)
+    expect(page).to have_link item.name, href: item_path(item)
     # 日記詳細ページに遷移する
-    visit item_path(@item)
+    visit item_path(item)
     # フォームに情報を入力する
-    fill_in 'item_comment[comment]', with: @item_comment.comment
+    fill_in 'item_comment[comment]', with: item_comment.comment
     # コメントを送信すると、Commentモデルのカウントが1上がることを確認する
     expect{
       find('input[name="commit"]').click
     }.to change { ItemComment.count }.by(1)
     # 詳細ページにリダイレクトされることを確認する
-    expect(current_path).to eq item_path(@item)
+    expect(current_path).to eq item_path(item)
     # 詳細ページ上に先ほどのコメント内容が含まれていることを確認する
-    expect(page).to have_content(@item_comment.comment)
+    expect(page).to have_content(item_comment.comment)
   end
 end
